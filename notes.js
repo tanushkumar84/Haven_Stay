23bb949a49642efd56ab5ef8750f742f6449e227
Tanush <tanushkumar84@gmail.com>
2024-07-03 07:26:42 +0530

    first commit

---
+++ notes.js
@@ -0,0 +1,70 @@
+//To Start any Project first we have to install all packages
+// npm init -y
+// npm install express
+// npm install ejs
+// npm install mongoose
+// npm install express-validator
+// npm install nodemon
+
+
+// then for github profile also upload your code time to time 
+//pushing local repo 
+//git init
+//git remote add origin <repository-url>
+//git status
+//git remote -v   (to verify remote)
+//git branch  (to check branch)
+//git branch -M main  (to remote branch)
+//git push  -u origin main
+//git add .
+//git commit -m "Initial commit"
+//git push origin main
+
+
+//git commit -am ("commit")
+//git push
+
+
+//then require and connect all package and data in your main file
+// const express = require('express');
+// const app = express();
+// const mongoose = require('mongoose');
+//const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+// const {check,validationResult} = require('express-validator');
+// const ejs = require('ejs');
+
+
+// then check you properly connect to your server
+// app.listen(3000,()=>{
+    //     console.log("Server is running on port 3000");
+    // });
+
+
+//then to MODEL: listing
+// like
+// title, description , image, price, location , country 
+
+
+//create route and page like new page show page edit page
+
+
+//What is EJS Mate
+//EJS is a templating engine that allows you to generate HTML markup with JavaScript. It stands
+//for Embedded JavaScript. It's a simple templating language that lets you generate HTML
+//on the server-side and then send it to the client's web browser. EJS is similar
+//to other templating engines like Pug, Handlebars, and Mustache.
+//EJS is often used with Node.js and Express.js to generate dynamic web pages on the server
+//side. It's a popular choice because it's easy to learn and use, and it's
+//well-suited for building web applications that require server-side rendering.
+//EJS templates are typically stored in separate files with a .ejs extension. These files
+//contain a mix of HTML and JavaScript code. The JavaScript code is used to
+//generate dynamic content, and the HTML code is used to define the structure
+//and layout of the page.
+//npm i ejs-mate
+//const ejsMate =require("ejs-mate")
+
+
+// for fonts - use google fonts
+//for icons - use fontawesome
+//for bootstrap mlibrary - use getbootstrap
+//for backend library - use npmjs.com
