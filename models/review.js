3951912720fa13a314674a304db2618f7448c642
tanushkumar84 <tanushkumar84@gmail.com>
2024-07-10 23:12:03 +0530

    commit

--- models/review.js
+++ models/review.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
- const Schema=mongoose.Schema;
- const reviewSchema=new Schema({
-    Comment:String,
-    rating:{
-        type:Number,
-        min:1,
-        max:5,
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    comment:String,
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
     },
-    createdAt:{
-        type:Date,
-        default:Date.now()
+    createdAt: {
+        type: Date,
+        default: Date.now()
     }
- });
- module.exports=mongoose.model("Review", reviewSchema); 
+});
+module.exports = mongoose.model("Review", reviewSchema); 
