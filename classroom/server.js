8372d63c016ee6dcaa4dc0be232081a5daebc237
tanushkumar84 <tanushkumar84@gmail.com>
2024-07-15 20:33:55 +0530

    commit

--- classroom/server.js
+++ classroom/server.js
@@ -2,13 +2,50 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
+const session = require("express-session");
+const flash =require("connect-flash")
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+
+const sessionOptions = {
+    secret: "mysupersecretstring", reverse: false, saveUninitialized: true,
+};
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.get("/registre",(req,res)=>{
+    let {name ="anonymous"}=req.query;
+    req.session.name=name;
+    req.flash("success","welcome to my site")
+    res.redirect("/hello");
 });
 
-app.use("/users", users);
-app.use("/posts", posts);
+app.get("/hello", (req, res) => {
+    res.send(`hllo , ${req.session.name}`);
+});
+// app.get ("/test",(req,res)=>{
+//     res.send("test successful");
+// })
+// const cookieParser= require("cookie-parser");
+// app.use(cookieParser("secretcode"));
+
+// app.get("/getcookies",(req,res)=>{
+//     res.cookie("greet","hello",{signed:true});
+//     res.send("sent you some cookies");
+// });
+
+// app.get("/verify",(req,res)=>{
+//     console.log(req.cookies);
+//     res.send("verified");
+// })
+
+// app.get("/greet", (req, res) => {
+//     let{name="anonymous"}=req.cookies;
+//     res.send(`hi,${name} `);
+// });
+
+// app.use("/users", users);
+// app.use("/posts", posts);
 
 
 app.listen(3000, () => {
