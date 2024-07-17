03e9e91a70544b9a50be82e0d90bcad421b3c23c
tanushkumar84 <tanushkumar84@gmail.com>
2024-07-17 15:53:15 +0530

    uplad images

--- routes/listing.js
+++ routes/listing.js
@@ -3,23 +3,28 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js"); // Corrected path
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-const listingController = require("../controllers/listings.js")
+const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const uplaod = multer({ dest: 'uplaods/' })
 
 //index and create route 
 router.
-route("/")
-.get(wrapAsync(listingController.index))
-.post( wrapAsync(listingController. createListing));
+    route("/")
+    .get(wrapAsync(listingController.index))
+    // .post( wrapAsync(listingController. createListing));
 
+    .post(uplaod.single('listing[image]'), (req, res) => {
+        res.send(req.file);
+    })
 //NEW ROUTE
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 
 //show, update and delete route
 router.route("/:id")
-.get( wrapAsync(listingController.showListing))
-.put( isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))
-.delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+    .get(wrapAsync(listingController.showListing))
+    .put(isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))
+    .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 
 //Edit ROUTE
